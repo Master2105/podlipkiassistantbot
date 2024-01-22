0ab56a81d991c136b93d119e7f22c221c444a8a6
Master2105 <67862254+Master2105@users.noreply.github.com>
2024-01-22 21:28:07 +0300

    Update secrets.py

--- bot/secrets.py
+++ bot/secrets.py
@@ -2,21 +2,21 @@ import os
 
 # #Railway configuration
 
-# bot_token = '5598904575:AAEcs_QJDOK3PoRXDQ6AjGlHkcpLzAderBI'
+# bot_token = ''
 
-# events_sheet_id = '1YTe--155Ot12nkgOenhipqGiltcpwU8eQEsPKI8xwQI'
+# events_sheet_id = ''
 # users_sheet_id = ''
-# googledoc_id = '10_pb1AUBjVWU9aXz6kc3_-dBmpr57pSig7N87M3FuMk'
+# googledoc_id = ''
 
 # path_to_service_json = '/app/.config/gspread/main_service_account.json'
 
 
 #Test
 
-bot_token = '5113437177:AAEzYsutywMozzzaYgw036tgo2TTXimFcjo'
+bot_token = ''
 
-events_sheet_id = '13jf2Hx9LAj7N3CwgWkDuzeZ31K4HAceONUPkawB4HOA'
+events_sheet_id = ''
 users_sheet_id = ''
-googledoc_id = '10_pb1AUBjVWU9aXz6kc3_-dBmpr57pSig7N87M3FuMk'
+googledoc_id = ''
 
 path_to_service_json = '.config/gspread/test_service_account.json'
